35c36555deb26049b598200a417f84ddcc08b976
Mark Stoecker <webmaster@desertwebdesigns.com>
2017-03-30 22:12:54 -0700

    EHR-2 #resolve #comment MDM calculator complete

--- js/util.js
+++ js/util.js
@@ -29,7 +29,9 @@ $(document).ready(function(){
 function calculateMDM() {
 	var MDMproblems = { "MDMminprob":1, "MDMstabprob":1, "MDMworseprob":2, "MDMnewprobno":3, "MDMnewprobyes":4 };
 	var MDMdata = { "MDMdatalab":1, "MDMdataradio":1, "MDMdatamedicine":1, "MDMdatadiscuss":1, "MDMdatarecdec":1, "MDMdatarecrev":2 };
-	var problem_types = {}, problem_points = 0, data_points = 0;
+	var MDMresults = { 1:"Straightforward", 2:"Low", 3:"Moderate", 4:"High" };
+	var MDMrisk = { "min":1, "low":2, "mod":3, "high":4 };
+	var problem_types = {}, problem_points = 0, data_points = 0, mdm = '', problem_result = ''; data_result = '';
 	
 	for (var key in MDMproblems) {
 		if (MDMproblems.hasOwnProperty(key)) {
@@ -54,5 +56,53 @@ function calculateMDM() {
 				data_points += (MDMdata[key])
 			}
 		}
+	}
+	
+	var risk_result = ($("#MDMform #MDMriskmenu").val()) ? MDMrisk[$("#MDMform #MDMriskmenu").val()] : 1;
+
+	switch (true) {
+		case (problem_points <= 1):
+			problem_result = 1;
+			break;
+		
+		case (problem_points == 2):
+			problem_result = 2;
+			break;
+		
+		case (problem_points <= 3):
+			problem_result = 3;
+			break;
+		
+		case (problem_points >= 4):
+			problem_result = 4;
+			break;
+	}
+
+	switch (true) {
+		case (data_points <= 1):
+			data_result = 1;
+			break;
+		
+		case (data_points == 2):
+			data_result = 2;
+			break;
+		
+		case (data_points <= 3):
+			data_result = 3;
+			break;
+		
+		case (data_points >= 4):
+			data_result = 4;
+			break;
+	}
+	
+	if ((problem_result == data_result) || (problem_result == risk_result)) {
+		mdm = MDMresults[problem_result];
+	} else if (data_result == risk_result) {
+		mdm = MDMresults[data_result];
+	} else {
+		var results = [problem_result, data_result, risk_result];
+		results.sort(function(a, b){return a-b});
+		mdm = MDMresults[results[1]];
 	}
 }
